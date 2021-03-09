3b91c606041b96dea635a33b9d404f2d056e0ba4
mingweiniu <mingweiniu@gmail.com>
2021-03-09 15:38:47 +0800

    Create 0002.cpp

---
+++ Algorithm/Leetcode/0002.cpp
@@ -0,0 +1,10 @@
+// 1108 Defanging an IP Address
+
+// https://leetcode.com/problems/defanging-an-ip-address/discuss/328855/C%2B%2B-1-liner-(regex_replace)
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        return regex_replace(address, regex("[.]"), "[.]");
+    }
+};
