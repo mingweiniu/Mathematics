9df32284503f41a8b674c7047b0b5ea7902d53d3
mingweiniu <mingweiniu@gmail.com>
2021-03-09 15:31:46 +0800

    Update 0001.cpp

--- Algorithm/Leetcode/0001.cpp
+++ Algorithm/Leetcode/0001.cpp
@@ -1,13 +1,5 @@
-# 1480 Running Sum of 1d Array
-
-## C++
+// 1480 Running Sum of 1d Array
 vector<int> runningSum(vector<int>& nums) {
     partial_sum(nums.begin(), nums.end(), nums.begin());
     return nums;
 }
-
-
-## Python3
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        return accumulate(nums)
