424f3f33b3204e22d3767c99820aaf116516553b
mingweiniu <mingweiniu@gmail.com>
2021-03-09 15:37:33 +0800

    Update 0001.cpp

--- Algorithm/Leetcode/0001.cpp
+++ Algorithm/Leetcode/0001.cpp
@@ -1,4 +1,7 @@
 // 1480 Running Sum of 1d Array
+
+//https://leetcode.com/problems/running-sum-of-1d-array/discuss/686276/C%2B%2BPython-Partial-Sum
+
 vector<int> runningSum(vector<int>& nums) {
     partial_sum(nums.begin(), nums.end(), nums.begin());
     return nums;
