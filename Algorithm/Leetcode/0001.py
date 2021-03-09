6aadd48235a99f81788cc33cc87cb790eab8a047
mingweiniu <mingweiniu@gmail.com>
2021-03-09 15:37:28 +0800

    Update 0001.py

--- Algorithm/Leetcode/0001.py
+++ Algorithm/Leetcode/0001.py
@@ -1,5 +1,6 @@
 # 1480 Running Sum of 1d Array
-""" Python3 """
+# https://leetcode.com/problems/running-sum-of-1d-array/discuss/686276/C%2B%2BPython-Partial-Sum
+"""py3"""
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         return accumulate(nums)
